50a4566d95011e607a3d8403ad2b8c148e8b836f
lary <laryssaesperpereira@gmail.com>
2024-11-19 15:13:31 -0300

    Aula 2

--- server.js
+++ server.js
@@ -1,15 +1,56 @@
 import express from "express";
-//importa o express
+
+const posts = [
+    {
+      id: 1,
+      descricao: 'Um gatinho fofo', 
+      imagem: 'https://placecats.com/400/300'
+    },
+    {
+      id: 2,
+      descricao: 'Gatinho explorando uma caixa',
+      imagem: 'https://placecats.com/300/200'
+    },
+    {
+      id: 3,
+      descricao: 'Olhos de gato brilhando no escuro',
+      imagem: 'https://placecats.com/400/300'
+    },
+    {
+      id: 4,
+      descricao: 'Gatinho ronronando no colo',
+      imagem: 'https://placecats.com/300/200'
+    },
+    {
+      id: 5,
+      descricao: 'Gatinho brincando com um novelo de lã',
+      imagem: 'https://placecats.com/400/300'
+    }
+  ];
 
 const app = express();
-//coloca a função express dentro da variável app
+
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log("Servidor Escutando...");
 });
-//app irá ouvir solicitações da porta 3000 (servidor local), que mostrará no console a frase
 
-//criando rota
-app.get("/api", (req, res) => {
-    res.status(200).send("Boas Vindas à Imersão!");
+// app.get("/api", (req, res) => {
+//     res.status(200).send("Boas Vindas à Imersão!");
+// });
+
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
+});
+
+function buscarPost (id){
+    return posts.findIndex((post)=>{
+        return post.id === Number(id)
+    })
+};
+
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPost(req.params.id);
+    res.status(200).json(posts[index]);
 });
